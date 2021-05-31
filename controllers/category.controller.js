e3691ced7ac1d62d77f1058f424567826881f661
Tuan Minh Ho <minhtuan.t1181@gmail.com>
2021-05-31 20:41:36 +0700

    crud for category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,22 +2,75 @@ const categoryModel = require('../models/category.model');
 
 module.exports = {
   addNewCategory: function (req, res) {
-    const { categoryName } = req.body;
-    console.log(categoryName);
-    if (!categoryName){
-      res.status(400).json("Category name is required!");
-      return;
+    const name  = req.body.name;
+    console.log(name);
+    if (!name){
+      return res.status(400).json("Category name is required!");
     }
-    //Create new category
-    const category = categoryModel.addCategory(categoryName);
+    const category = categoryModel.addCategory(name);
     console.log(category);
     if (!category && !category.id){
-      res.status(500).json("Please try again later");
-      return;
+      return res.status(500).json("Please try again later");
     }
     res.status(201).json({
       message: "Create category successfully!",
       data: category,
     });
+  },
+
+  getAllCategory: function (req, res) {
+    categoryModel.getAllCategory((err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      return res.json(result);
+    })
+  },
+  
+  getCategoryById: function (req, res) {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json("Category Id is required");
+    }
+    categoryModel.getCategoryById(id, (err, result) => {
+      if (err) {
+        return res.status(400).json(err.message);
+      }
+      if (!result.length) {
+        return res.status(204).json();
+      }
+      res.status(200).json(result);
+    });
+  },
+
+  updateCategory: function (req, res) {
+    const id = req.params.id;
+    const { name }= req.body;
+    if (!id) {
+      return res.status(400).json("Category id is required!");
+    }
+    if (!name) {
+      return res.status(400).json("Category name is required!");
+    }
+    categoryModel.updateCategory(id, name, (err, result) => {
+      if (err) {
+        return res.status(500).json(err.message);
+      }
+      return res.status(200).json("Update category successfully");
+    })
+  },
+
+  deleteCategory: async function (req, res) {
+    const id = req.params.id;
+    try {
+      const category = await categoryModel.deleteCategory(id);
+      if (!category) {
+        return res.status(204).json();
+      }
+    return res.status(200).json("Delete category successfully");
+    } catch (error) {
+      return res.status(500).json();
+    }
+    
   }
 }
--- models/category.model.js
+++ models/category.model.js
@@ -21,6 +21,64 @@ CategorySchema.statics.addCategory = function (categoryName) {
   return newCategory;
 }
 
+CategorySchema.statics.getAllCategory = function (callBack) {
+  this.find({}, (error, result) => {
+    if(error) {
+        callBack(err);
+    }
+    else {
+      callBack(null, result);
+    }
+  });
+}
+
+CategorySchema.statics.getCategoryById = function (categoryId, callBack) {
+  this.find({_id: categoryId })
+    .then((value) => {
+      if (!value) {
+        callBack({
+          message: {
+            msgBody: 'No category found!',
+            msgError: true,
+          },
+        });
+      } else {
+        callBack(null, value);
+      }
+    })
+    .catch((err) => {
+      callBack(err);
+    });
+};
+
+CategorySchema.statics.updateCategory = function (id, categoryName, callBack) {
+  this.findOne({ _id: id })
+    .then((document) => {
+      if (!document) {
+        callBack({
+          message: {
+            msgBody: 'Can not found category!',
+            msgError: true,
+          },
+        });
+      } else {
+        this.update({ _id: id }, {name: categoryName}, (err, doc) => {
+          if (err) {
+            callBack(err);
+          } else {
+            callBack(null, doc);
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      callBack(err);
+    });
+}
+
+CategorySchema.statics.deleteCategory = async function (id) {
+  return await this.findByIdAndDelete(id);
+}
 CategorySchema.set('toObject', { getters: true });
 CategorySchema.set('toJSON', { getters: true });
 
