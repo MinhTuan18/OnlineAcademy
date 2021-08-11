92f9ec46dfb870d2dbf88f697ac549f5843ce9d2
Tuan Minh Ho <minhtuan.t1181@gmail.com>
2021-08-12 01:03:10 +0700

    add route index

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,3 @@ module.exports.feedbackRoute = require('./feedback.route');
 module.exports.chapterRoute = require('./chapter.route')
 module.exports.watchListRoute = require('./watchlist.route');
 module.exports.registeredCourseRoute = require('./registered-course.route');
-
-
