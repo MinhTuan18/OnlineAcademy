4831afcb661f989b859e1f2f7f0b385c4d94d8ae
Tuan Minh Ho <minhtuan.t1181@gmail.com>
2021-08-20 16:47:15 +0700

    hotfix: remove duplicate func create course + watchlist router

--- routes/watchlist.route.js
+++ routes/watchlist.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { watchListcontroller } = require('../controllers');
-const auth = require('../middlewares/auth.mdw');
+const {auth} = require('../middlewares/auth.mdw');
 const router = express.Router();
 
 router.post('/:id', auth, watchListcontroller.addNewCourseToWatchList);
